42c18d30541e089d70e61a073f2b3487e3bf74fe
Liam Moran <moran@illinois.edu>
2026-05-26 17:19:19 -0500

    Prettified timing report

--- chat.py
+++ chat.py
@@ -7,12 +7,38 @@ import traceback
 import urllib.request
 
 
+def secs2time(secs):
+    if secs <= 0.01:
+        return 'no time at all'
+    hours = str(secs // (60 * 60)).split('.')[0]
+    if hours == '0':
+       hours = ''
+    else:
+       hours = hours + ' hours, '
+    secs = secs % (60 * 60)
+    minutes = str(secs // 60).split('.')[0]
+    if minutes == '0':
+        minutes = ''
+    else:
+        minutes = minutes + ' minutes, '
+    secs = str(secs % 60)
+    secs = secs.split('.')
+    if len(secs) == 1 or secs[1] == '0':
+        secs = secs[0] + ' seconds'
+    else:
+        secs[1] = secs[1][:2]
+        secs = '.'.join(secs) + ' seconds'
+    return ''.join([hours, minutes, secs])
+
 def main():
     while (message := input('Type a message to send, quit, or reload: ')).lower() != 'quit':
         if message.lower() == 'reload':
             try:
                 importlib.reload(vars)
                 print("Reloaded variables!")
+                print("Model: " + vars.model)
+                print("Temp: " + str(vars.temperature))
+                print("System Prompt:\n" + vars.system_prompt + '\n\n')
             except Exception as err:
                 print('Error reloading variables!\n#########\n\n')
                 print(traceback.print_exception(err))
@@ -36,12 +62,13 @@ def main():
             req = urllib.request.Request(url, json.dumps(data).encode('UTF-8'),headers,method="POST")
             with urllib.request.urlopen(req) as resp:
                 response = resp.read().decode('UTF-8', errors='ignore')
+                # Qwen models prepend the response with a <think> el
                 response = response.split('<think>')[-1].split('</think>')
                 if len(response) == 2:
                     print('Internal monologue:\n' + response[0] + '\n\nMessage:\n' + response[1])
                 else:
                     print('Message:\n' + response[0])
-                print('Round trip duration: ' + str(round(time.time() - before, 2)) + ' seconds\n')
+                print('Round trip duration: ' + secs2time(time.time() - before) + '\n')
                 
                 
 
